23bbbbf1605be008be95490be8c6c2c3335f1a9e
zhtordinary <zhuhaitao@tongji.edu.cn>
2020-03-22 18:21:59 +0800

    上海中心

---
+++ 上海中心/MySQL/start mySQL.bat
@@ -0,0 +1,10 @@
+@ echo off
+%1 %2
+ver|find "5.">nul&&goto :Admin
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof
+:Admin
+
+net start mysql
+mysql -u root -p
+show databases;
+pause
